1600f5b4da0f7c38b37708b2cc17df304835932d
Manuel Cruz <cruzmanuelar@gmail.com>
2024-06-22 08:15:48 -0500

    2106 CA: Url backend

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    apiUrl : "https://back-ca-2024.onrender.com",
+    apiUrl : "https://back-ca-2024.onrender.com/",
     // apiUrl : "https://cruzmanueltech.com/"
     // apiUrl: "http://localhost:3001/"
 }
